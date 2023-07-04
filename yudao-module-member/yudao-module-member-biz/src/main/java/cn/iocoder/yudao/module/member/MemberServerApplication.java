bfc24821a52c9fa9f519357d55af74efa0678ba1
lirenhao <renhao.li@bjyada.com>
2023-07-04 15:50:27 +0800

    添加会员模块

---
+++ yudao-module-member/yudao-module-member-biz/src/main/java/cn/iocoder/yudao/module/member/MemberServerApplication.java
@@ -0,0 +1,12 @@
+package cn.iocoder.yudao.module.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberServerApplication.class, args);
+    }
+}
