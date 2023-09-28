004a406fba7757942443d5fa75c7b5d68ecec092
lirenhao <renhao.li@bjyada.com>
2023-09-28 09:16:37 +0800

    构建social

---
+++ like-framework/like-spring-boot-starter-biz-social/src/main/java/cn/like/cloud/framework/social/config/LikeSocialAutoConfiguration.java
@@ -0,0 +1,33 @@
+package cn.like.cloud.framework.social.config;
+
+import cn.like.cloud.framework.social.core.LikeAuthRequestFactory;
+import com.xingyuv.http.HttpUtil;
+import com.xingyuv.http.support.hutool.HutoolImpl;
+import com.xingyuv.jushauth.cache.AuthStateCache;
+import com.xingyuv.justauth.autoconfigure.JustAuthProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+/**
+ * 社交自动装配类
+ */
+@Slf4j
+@AutoConfiguration
+@EnableConfigurationProperties(JustAuthProperties.class)
+public class LikeSocialAutoConfiguration {
+
+    @Bean
+    @Primary
+    @ConditionalOnProperty(prefix = "justauth", value = "enabled", havingValue = "true", matchIfMissing = true)
+    public LikeAuthRequestFactory likeAuthRequestFactory(JustAuthProperties properties, AuthStateCache authStateCache) {
+        // 需要修改 HttpUtil 使用的实现，避免类报错
+        HttpUtil.setHttp(new HutoolImpl());
+        // 创建 LikeAuthRequestFactory
+        return new LikeAuthRequestFactory(properties, authStateCache);
+    }
+
+}
